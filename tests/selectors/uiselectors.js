00d38a5471200eb4e0dbe8b430cef251d2df656b
Naeem Malik <naeem.akram.malik@gmail.com>
2024-05-07 11:41:32 +0500

    How to seperate selector strings from PlayWright test scripts for reusibility and clarity.

---
+++ tests/selectors/uiselectors.js
@@ -0,0 +1,9 @@
+export const whatNeedsToBeDone = 'What needs to be done?';
+export const getSomeMilk = 'Get some milk.';
+export const todoTitle = "todo-title";
+export const writeMoreCode = 'Write more code';
+export const getYourActTogether = 'Get your act together';
+export const all = 'All';
+export const enter = 'Enter';
+export const body = 'body';
+export const link = 'link';
